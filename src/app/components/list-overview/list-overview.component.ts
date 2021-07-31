73fafdfe7d3762b1c24eeeb9f0c9c3e56e6ee416
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-07-31 02:46:07 -0700

    add link to list

--- src/app/components/list-overview/list-overview.component.ts
+++ src/app/components/list-overview/list-overview.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/firestore";
+import { ColorService } from "src/app/services/color.service";
 import { TodoList } from "../../types";
 
 @Component({
@@ -17,25 +18,29 @@ export class ListOverviewComponent implements OnInit {
 
   public isInLightMode: boolean = true;
 
-  constructor(private firestore: AngularFirestore) {}
+  constructor(
+    private firestore: AngularFirestore,
+    public color: ColorService
+  ) {}
 
-  getTextColor(backgroundHex: string): string {
-    if (backgroundHex == null) {
-      return "white";
-    }
-    var r = parseInt(backgroundHex.substr(1, 2), 16);
-    var g = parseInt(backgroundHex.substr(3, 2), 16);
-    var b = parseInt(backgroundHex.substr(5, 2), 16);
-    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
-    this.isInLightMode = yiq < 128;
-    return yiq >= 128 ? "black" : "white";
-  }
+  // getTextColor(backgroundHex: string): string {
+  //   if (backgroundHex == null) {
+  //     return "white";
+  //   }
+  //   var r = parseInt(backgroundHex.substr(1, 2), 16);
+  //   var g = parseInt(backgroundHex.substr(3, 2), 16);
+  //   var b = parseInt(backgroundHex.substr(5, 2), 16);
+  //   var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+  //   this.isInLightMode = yiq < 128;
+  //   return yiq >= 128 ? "black" : "white";
+  // }
 
   ngOnInit(): void {
     this.firestore
       .doc<TodoList>(`lists/${this.id}`)
       .valueChanges()
       .subscribe((val) => {
+        this.isInLightMode = this.color.getTextColor(val?.color!) === "black";
         this.list = val!;
         this.doneCount = val?.items.filter((item) => item.done).length;
         this.notDoneCount = val?.items.length! - this.doneCount!;
