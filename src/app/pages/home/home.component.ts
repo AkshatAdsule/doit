68c974a1139028b6eb9b388066fedd45da613603
Akshat Adsule <30205531+AkshatAdsule@users.noreply.github.com>
2021-07-31 02:47:03 -0700

    add create list option

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore } from "@angular/fire/firestore";
-import { User } from "../../types";
+import { Router } from "@angular/router";
+import { TodoList, User } from "../../types";
 
 @Component({
   selector: "app-home",
@@ -10,21 +11,43 @@ import { User } from "../../types";
 })
 export class HomeComponent implements OnInit {
   public lists?: string[];
+  private email: string = "";
+  private uid: string = "";
   constructor(
     private firestore: AngularFirestore,
-    private auth: AngularFireAuth
+    private auth: AngularFireAuth,
+    private router: Router
   ) {}
 
   async ngOnInit(): Promise<void> {
     let current_user: firebase.default.User | null = await this.auth
       .currentUser;
-    let email = current_user?.email;
+    this.uid = current_user?.uid!;
+    this.email = current_user?.email!;
 
     this.firestore
-      .doc<User>(`/users/${email}`)
+      .doc<User>(`/users/${this.email}`)
       .valueChanges()
       .subscribe((doc) => {
         this.lists = doc?.lists!;
+      });
+  }
+
+  public new() {
+    this.firestore
+      .collection<TodoList>("/lists")
+      .add({
+        owner: this.uid,
+        title: "New List",
+        color: "#" + Math.floor(Math.random() * 16777215).toString(16),
+        editors: [this.uid],
+        items: [],
+      })
+      .then(async (doc) => {
+        await this.firestore
+          .doc<User>(`/users/${this.email}`)
+          .update({ lists: [...this.lists!, doc.id] });
+        this.router.navigateByUrl(`/list/${doc.id}`);
       });
   }
 }
